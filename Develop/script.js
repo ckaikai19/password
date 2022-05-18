08bcc9295804565e1c4b9080608794636532fd1e
chirstiankaikai <ckaikai19@gmail.com>
2022-05-18 15:37:32 -0400

    added code w/o comments

--- Develop/script.js
+++ Develop/script.js
@@ -1,17 +1,52 @@
-// Assignment code here
 
-
-// Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
+var textArea = document.getElementById('password')
 
-// Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  var passwordLength = parseInt(prompt('Enter number between 8 and 128 characters for password length'));
+  
+  var includesLowerCase = confirm('Do you want to include lowercase characters'),
+      includesUpperCase = confirm('Do you want to include uppercase characters'),
+      includesNumeric = confirm('Do you want to include numeric characters'),
+      includesSpecial = confirm('Do you want to include special characters');
 
-  passwordText.value = password;
+  var lowercase = 'abcdefghijklmnopqrstuvwxyz',
+      uppercase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ',
+      numeric = '1234567890',
+      special = '!@#$%^&*()_+{}|":?><~-`';
 
+  var avaibleCharacters = '',
+      password = '';
+
+
+  if(!includesLowerCase && !includesUpperCase && !includesNumeric && !includesSpecial) {
+    alert("You must include atleast one character type, please refresh the page");
+  } else {
+    console.log('continue');
+  }
+
+  if(includesLowerCase){
+    avaibleCharacters += lowercase;
+  }
+
+  if(includesUpperCase){
+    avaibleCharacters += uppercase;
+  }
+
+  if(includesNumeric){
+    avaibleCharacters += numeric;
+  }
+
+  if(includesSpecial){
+    avaibleCharacters += special
+  }
+
+  for (var i = 0; i <= passwordLength; i++) {
+    password += avaibleCharacters.charAt(Math.floor(Math.random() * avaibleCharacters.length));
+  }
+
+  textArea.innerText = password;
 }
 
-// Add event listener to generate button
+
 generateBtn.addEventListener("click", writePassword);
